16a590f60469096f1d6152d0201b1f33ae2dc6af
Paul Fell <swellpf@gmail.com>
2018-09-23 00:30:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def take_a_number(katz_deli,name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.size = 0
+  if katz_deli.size = NIL
     puts "There is nobody waiting to be served!"
   else  
     next_person = katz_deli.shift
